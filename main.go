109dfb57ec7f5ca9cdd2f8f1dc973702b7a857d7
rorycl <rorycl@campbell-lange.net>
2025-03-22 22:14:20 +0000

    update

--- main.go
+++ main.go
@@ -11,19 +11,29 @@ import (
 	flags "github.com/jessevdk/go-flags"
 )
 
-var regexReplace = regexp.MustCompile("[-_,&()+@ ]+")
+var regexReplace = regexp.MustCompile("[-_:,&()+@ ]+")
 
 // Options are the command line options
 type Options struct {
 	Verbose bool `short:"v" long:"verbose" description:"verbose"`
+	Test    bool `short:"t" long:"testmode" description:"enter test mode"`
 	Args    struct {
 		DirOrFilePath string `description:"directory path to process"`
 	} `positional-args:"yes" required:"yes"`
 }
 
+type funcRenamer func(oldpath, newpath string) error
+
+var FileRenamer funcRenamer = os.Rename
+
+func printRenamer(oldpath, newpath string) error {
+	fmt.Printf("rename %s -> %s\n", oldpath, newpath)
+	return nil
+}
+
 var usage = fmt.Sprintf(`DirPath
 
-Recursively rename all files and directories at and under DirPath.
+Recursively rename all files and directories at and under DirOrFilePath.
 Renaming lowercases all files and directories and replaces the
 characters "%s" with the replacement character "_".`, regexReplace)
 
@@ -45,7 +55,7 @@ func pathRenamer(path string, verbose, isDir bool) (fn string, err error) {
 	newFile = strings.TrimRight(newFile, "_")
 	newFile = strings.ToLower(newFile)
 	newFile = strings.ReplaceAll(newFile, "_.", ".")
-	fn = fileDir + newFile
+	fn = filepath.Join(fileDir, newFile)
 
 	// directories can be seen twice
 	if isDir {
@@ -67,7 +77,12 @@ func pathRenamer(path string, verbose, isDir bool) (fn string, err error) {
 		}
 		return
 	}
-	return fn, os.Rename(fileDir+file, fn)
+
+	_, err = os.Stat(fn)
+	if err == nil {
+		return fn, fmt.Errorf("file %s already exists", fn)
+	}
+	return fn, FileRenamer(path, fn)
 }
 
 func main() {
@@ -80,30 +95,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	dir := filepath.Clean(options.Args.DirPath)
+	// swap out to printer if in test mode
+	if options.Test {
+		FileRenamer = printRenamer
+	}
 
-	info, err := os.Stat(dir)
+	dirOrFile := filepath.Clean(options.Args.DirOrFilePath)
+
+	info, err := os.Stat(dirOrFile)
 	if err != nil {
-		fmt.Printf("error reading dir: %s\n", dir)
+		fmt.Printf("error reading input directory/file : %s\n", dirOrFile)
 		os.Exit(1)
 	}
-	if !info.IsDir() {
-		fmt.Printf("%s is not a directory", dir)
-		os.Exit(1)
+	if options.Verbose {
+		fmt.Printf("%s -> isdir() %v\n", dirOrFile, info.IsDir())
 	}
 
 	// rename root if necessary
-	newDir, err := pathRenamer(dir, options.Verbose, info.IsDir())
+	newDirOrFile, err := pathRenamer(dirOrFile, options.Verbose, info.IsDir())
 	if err != nil {
-		fmt.Printf("error renaming root %s: error %s\n", dir, err)
+		fmt.Printf("error renaming %s: %v\n", dirOrFile, err)
 		os.Exit(1)
 	}
 
+	if !info.IsDir() {
+		return
+	}
 	// process tree twice, first for files, second for directories
 	for _, dirMode := range []bool{false, true} {
-		filepath.WalkDir(newDir, func(path string, info fs.DirEntry, err error) error {
-			if dirMode == info.IsDir() {
-				_, err = pathRenamer(path, options.Verbose, info.IsDir())
+		filepath.WalkDir(newDirOrFile, func(path string, i fs.DirEntry, err error) error {
+			if dirMode == i.IsDir() {
+				_, err = pathRenamer(path, options.Verbose, i.IsDir())
 				if err != nil {
 					fmt.Printf("%s: error %s\n", path, err)
 					os.Exit(1)
