76a84d1d6e4603fc7cae7313dece5d32ae0c50fb
rorycl <rorycl@campbell-lange.net>
2025-03-29 13:32:28 +0000

    operator: add cleaned path to processKind func

--- operator.go
+++ operator.go
@@ -1,27 +1,5 @@
 package main
 
-// operator (which isn't a great name) determines if the provided path
-// exists, if it is a file, a directory, or a directory for transversal.
-// These accord with the following path patterns:
-//
-//	// a file, type FILE
-//	1. /here/filename
-//	   ./here/filename
-//     filename
-//
-//  // a directory only (or "naked" directory), type DIR
-//  2. /here/directory
-//     ./here/directory
-//     directory
-//
-//  // a directory for walking, type WALK
-//  3. /here/directory/
-//     ./here/directory/
-//     directory/
-//
-// The difference between cases two and three is simply the final
-// filepath.Separator in the third case.
-
 import (
 	"fmt"
 	"os"
@@ -38,22 +16,47 @@ const (
 	WALK
 )
 
-func processKind(path string) (processType, error) {
+// processKind (which isn't a great name) determines if the provided
+// path exists, if it is a file, a directory, or a directory for
+// transversal. These accord with the following path patterns:
+//
+//	// a file, type FILE
+//	1. /here/filename
+//	   ./here/filename
+//	   filename
+//
+//	 // a directory only (or "naked" directory), type DIR
+//	 2. /here/directory
+//	    ./here/directory
+//	    directory
+//
+//	 // a directory for walking, type WALK
+//	 3. /here/directory/
+//	    ./here/directory/
+//	    directory/
+//
+// The difference between cases two and three is simply the final
+// filepath.Separator in the third case.
+func processKind(path string) (string, processType, error) {
 
 	var pt processType
 	hasTrailingSep := strings.HasSuffix(path, string(filepath.Separator))
+
+	// note that os.Stat is _not_ run on the cleaned path since the path
+	// provided to processKind is meaningful with a trailing Separator.
 	info, err := os.Stat(path)
 	if err != nil {
-		return pt, fmt.Errorf("process stat error for path %s: %w", path, err)
+		return "", pt, fmt.Errorf("process stat error for path %s: %w", path, err)
 	}
 	isDir := info.IsDir()
+	cleanedPath := filepath.Clean(path)
 
 	switch {
 	case !isDir:
-		return FILE, nil
+		return cleanedPath, FILE, nil
 	case isDir && !hasTrailingSep:
-		return DIR, nil
+		return cleanedPath, DIR, nil
 	default:
-		return WALK, nil
+		return cleanedPath, WALK, nil
 	}
 }
--- operator_test.go
+++ operator_test.go
@@ -8,45 +8,51 @@ import (
 func TestProcessKind(t *testing.T) {
 
 	tests := []struct {
-		path   string
-		result processType
-		isErr  bool
+		path        string
+		cleanedPath string
+		result      processType
+		isErr       bool
 	}{
 		{
-			path:   "testdata/A/%^&*()(___and",
-			result: FILE,
-			isErr:  false,
+			path:        "testdata/A/%^&*()(___and",
+			cleanedPath: "testdata/A/%^&*()(___and",
+			result:      FILE,
+			isErr:       false,
 		},
 		{
-			path:   "testdata/b 1&2",
-			result: DIR,
-			isErr:  false,
+			path:        "testdata/b 1&2",
+			cleanedPath: "testdata/b 1&2",
+			result:      DIR,
+			isErr:       false,
 		},
 		{
-			path:   "testdata/b 1&2/",
-			result: WALK,
-			isErr:  false,
+			path:        "testdata/b 1&2/",
+			cleanedPath: "testdata/b 1&2", // note stripped slash
+			result:      WALK,
+			isErr:       false,
 		},
 		{
-			path:   "testdata/doesntExist", // does not exist
-			result: NONE,
-			isErr:  true,
+			path:        "testdata/doesntExist", // does not exist
+			cleanedPath: "",
+			result:      NONE,
+			isErr:       true,
 		},
 		{
-			path:   "testdata/A/%^&*()(___and/", // not a directory
-			result: NONE,
-			isErr:  true,
+			path:        "testdata/A/%^&*()(___and/", // not a directory
+			cleanedPath: "",
+			result:      NONE,
+			isErr:       true,
 		},
 	}
 
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("test_%d", i), func(t *testing.T) {
-			result, err := processKind(tt.path)
-			if err != nil {
-				fmt.Println(err)
-			}
+			cleanedPath, result, err := processKind(tt.path)
 			if got, want := (err != nil), tt.isErr; got != want {
 				t.Fatalf("unexpected error %v", err)
+			}
+			if got, want := cleanedPath, tt.cleanedPath; got != want {
+				t.Errorf("cleanedPath got %s want %s", got, want)
 			}
 			if got, want := result, tt.result; got != want {
 				t.Errorf("result got %d want %d", got, want)
